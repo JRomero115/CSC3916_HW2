de15eb01b978e6f7f359442359013b89c57c2790
Janine Romero <janine.romero@ucdenver.edu>
2022-02-25 17:59:40 -0700

    update Movies

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ router.route('/movies')
                 res = res.type(req.get('Content-Type'));
             }
             var o = getJSONObjectForMovieRequirement(req);
-            res.json(o.headers, req.query, o.key);
+            res.status(200).send({headers: o.headers, msg: 'GET movies', headers: o.headers, query: req.query, env: o.key});
         }
     )
     .post(function (req, res) {
